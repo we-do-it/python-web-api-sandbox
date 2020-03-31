2c7d85fe89ed0e734527b740842efd56d07daed8
Filip Vanhoorelbeke <filip.vanhoorelbeke@partena.be>
2020-03-31 21:33:25 +0200

    Basic web api with bonjours

---
+++ api/app.py
@@ -0,0 +1,43 @@
+import flask
+from flask import jsonify, request
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+baseUrl = '/api/v1/'
+
+bonjours = [
+    {'id': 1, 'description': 'Bonjour!', 'language': 'fr'},
+    {'id': 2, 'description': 'Hallo!', 'language': 'nl'},
+    {'id': 3, 'description': 'Hello!', 'language': 'en'}
+]
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>Hi all! Please stay in your kot!</h1>"
+
+
+@app.route(baseUrl + 'bonjours/all', methods=['GET'])
+def bonjour_all():
+    return jsonify(bonjours)
+
+
+@app.route(baseUrl + 'bonjours', methods=['GET'])
+def bonjour_language():
+    # check for provided language parameter in request
+    if 'language' in request.args:
+        language = request.args['language']
+    else:
+        return "Please provide a language"
+
+    result = []
+
+    for bonjour in bonjours:
+        if bonjour['language'] == language:
+            result.append(bonjour)
+
+    return jsonify(result)
+
+
+app.run()
